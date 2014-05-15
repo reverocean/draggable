4ba4827eb158f25140ffbcd02b38e2c74e22b349
Haiyang He <hyhe@thoughtworks.com>
2014-05-15 15:02:06 +0800

    abc

--- test/unit/personSpec.js
+++ test/unit/personSpec.js
@@ -9,15 +9,39 @@ var Person = function (name, $log) {
 };
 
 
+var Restaurant = function($q, $rootScope) {
+    var currentOrder;
+
+    this.takeOrder = function(orderItems){
+        currentOrder = {
+            deferred: $q.defer(),
+            items: orderItems
+        }
+        return currentOrder.deferred.promise;
+    }
+
+    this.deliverOrder = function(){
+        currentOrder.deferred.resolve(currentOrder.items);
+        $rootScope.$digest();
+    }
+
+    this.problemWithOrder = function(){
+        currentOrder.deferred.reject('abc');
+        $rootScope.$digest();
+    }
+};
+
+
 describe('$q', function(){
     var $q, $log, $rootScope;
-    var person;
+    var person, restaurant;
 
     beforeEach(inject(function (_$q_, _$log_, _$rootScope_) {
         $q = _$q_;
         $log = _$log_;
         $rootScope = _$rootScope_;
         person = new Person('Pawel', $log);
+        restaurant = new Restaurant($q, $rootScope);
     }));
 
     it('should', function(){
@@ -28,5 +52,17 @@ describe('$q', function(){
         pizzaOrderFulfillment.resolve('Margherita');
         $rootScope.$digest();
         expect($log.info.logs).toContain(['Pawel is eating delicious Margherita']);
+    })
+
+    it('should restaurant', function(){
+        var takeOrder = restaurant.takeOrder(['a', 'b']);
+        takeOrder.then(function(items){
+            expect(items).toEqual(['a', 'b']);
+        }, function(reason){
+            expect(reason).toEqual('abc');
+        })
+
+//        restaurant.deliverOrder();
+        restaurant.problemWithOrder();
     })
 })

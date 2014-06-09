fcd3cddb3dbbe41fcb8db40699ea26354f04dba9
Haiyang He <hyhe@thoughtworks.com>
2014-06-09 21:39:48 +0800

    add simple bind and simple model directive

--- app/js/app.js
+++ app/js/app.js
@@ -7,8 +7,7 @@ angular.module('myApp', [
     'myApp.filters',
     'myApp.services',
     'myApp.directives',
-    'myApp.controllers',
-    'templates'
+    'myApp.controllers'
 ]).
     config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
--- app/js/directives.js
+++ app/js/directives.js
@@ -4,8 +4,33 @@
 
 
 angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+    directive('appVersion', ['version', function (version) {
+        return function (scope, elm, attrs) {
+            elm.text(version);
+        };
+    }]).
+    directive('simpleModel', ['$parse', function ($parse) {
+        return function (scope, element, attrs) {
+            var modelGetter = $parse(attrs.simpleModel);
+            var modelSetter = modelGetter.assign;
+
+            element.bind('input', function () {
+                scope.$apply(function(){
+                    modelSetter(scope, element.val());
+//                    scope.name = element.val();
+                });
+            });
+        };
+
+    }]).
+    directive('simpleBind', ['$parse', function($parse){
+        return function(scope, element, attrs){
+            var modelGetter = $parse(attrs.simpleBind);
+            scope.$watch(modelGetter, function (newValue, oldValue) {
+                element.text(modelGetter(scope));
+            });
+
+
+        };
+    }])
+;

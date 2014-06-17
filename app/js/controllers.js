999aacbc291980d39277a470d0cb715bc71b8f31
Haiyang He <hyhe@thoughtworks.com>
2014-06-17 13:16:05 +0800

    add pagination

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -16,22 +16,36 @@ angular.module('myApp.controllers', [])
         $scope.pageObject = {
             totalPage: 5,
             currentPage: 0,
-            elements: []
+            elements: [1, 2, 3 ,4 ,5 , 6]
         };
 
         $scope.isNotFirstPage = function(){
             return $scope.pageObject.currentPage != 0;
-        }
+        };
 
         $scope.isNotLastPage = function(){
-            return $scope.pageObject.currentPage != $scope.totalPage - 1;
+            return $scope.pageObject.currentPage != $scope.pageObject.totalPage - 1;
         }
 
         $scope.nextPage = function(){
+            if($scope.pageObject.currentPage <= ($scope.pageObject.totalPage -1)) {
+                $scope.pageObject.currentPage += 1;
+                $scope.pageObject.elements = []
+                for(var i = 1;i<= 6;i++){
+                    $scope.pageObject.elements.push(i + 6 * $scope.pageObject.currentPage);
+                }
 
+            }
         }
 
         $scope.previousPage = function(){
-
+            if($scope.pageObject.currentPage >= 0) {
+                $scope.pageObject.currentPage -= 1;
+                var elements = $scope.pageObject.elements;
+                $scope.pageObject.elements = [];
+                for(var i = 1;i<= 6;i++){
+                    $scope.pageObject.elements.push(elements[i-1] - 6);
+                }
+            }
         }
     }]);
